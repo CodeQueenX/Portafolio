9ec7d0d5264ec95ef63c134788a32ce6f970aec3
CodeQueenX <megidorico@gmail.com>
2025-09-01 18:01:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll suave al hacer clic en los enlaces del menú
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
